3447676a35d7134cd7bb5eb7f2f067a50fefc71a
alexx1307 <alexx1307@gmail.com>
2014-03-04 15:57:22 +0100

    Card struct

---
+++ Card.hpp
@@ -0,0 +1,32 @@
+enum Suit
+{
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+};
+
+enum Rank
+{
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
+
+struct Card
+{
+    Suit suit;
+    Rank rank;
+};
+
