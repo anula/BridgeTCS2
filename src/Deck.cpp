ad907b999c0fdc4d540fff491ca0e373c44f3072
laiqu <laiquendir@gmail.com>
2014-03-11 15:14:27 +0100

    Standard 52 cards deck.

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -1,3 +1,5 @@
+#include "Deck.hpp"
+
 void Deck::shuffle()
 {
     std::random_shuffle(deck.begin(), deck.end());
@@ -11,4 +13,15 @@ Card Deck::getCard()
 int Deck::count()
 {
     return deck.size();
+}
+
+Standard52Deck::Standard52Deck()
+{
+    for(int i = ONE; i <= ACE; i++)
+    {
+        for(int j = CLUBS; j <= SPADES; j++)
+        {
+            deck.push_back(Card(static_cast<Rank>(i), static_cast<Suit>(j)));
+        }
+    }
 }
--- src/Deck.hpp
+++ src/Deck.hpp
@@ -2,10 +2,11 @@
 #include <algorithm>
 #include <vector>
 #include "Card.hpp"
+
 class IDeck
 {
 public:
-    virtual ~IDeck() {}
+    virtual ~IDeck() {};
     virtual void shuffle() = 0;
     virtual Card getCard() = 0;
     virtual int count() = 0;
@@ -14,10 +15,19 @@ public:
 class Deck : public IDeck
 {
 public:
+    virtual ~Deck() = 0;
     void shuffle();
     Card getCard();
     int count();
-private:
+protected:
     std::vector<Card> deck;
-    Deck() {}
+};
+
+Deck::~Deck() {}
+
+class Standard52Deck : public Deck 
+{
+public:
+    ~Standard52Deck() {}
+    Standard52Deck(); 
 };
