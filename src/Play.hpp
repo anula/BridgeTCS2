6a9f257192d9f43331920c566e76ab6a6c61e111
zajaczajac <emzajac@gmail.com>
2014-03-11 15:52:23 +0100

    poprawki w deklaracjach i wstepna implementacja klasy Play

--- src/Play.hpp
+++ src/Play.hpp
@@ -1,30 +1,34 @@
 #ifndef PLAY_HPP
 #define PLAY_HPP
 
-#ifndef TRICK_HPP
 #include "Trick.hpp"
-#endif
+#include <array>
 
 class Play
 {
 public:
-	Play();
+	/* Przyjmuje: kolor atutowy, ID rozgrywajacego.
+	 */
+	Play(Trump trump, int firstPlayerID);
 	
-	/*
-	 * Przyjmuje: ID gracza, ktory zaczyna.
+	/* Przeprowadza rozgrywke.
+	 */
+	void play();	
+	/* Zwraca liczbe lew zebranych przez pare rozgrywajaca.
+	 */
+	int getResult();
+private:
+    using Trick = std::array<Card, 4>;
+    
+    Trump trump;
+    int beginningPlayer; // gracz zaczynajacy nastepna lewe
+    /* Przyjmuje: ID gracza, ktory zaczyna.
 	 *
 	 * Wysyla do kolejnych graczy zadania dolozenia kart do lewy.
 	 *
 	 * Zwraca: lewe wypelniona kartami.
 	 */
 	 Trick newTrick(int PlayerID);
-	
-	/*
-	 *	Zwraca liczbe lew zebranych przez pare rozgrywajaca.
-	 */
-	int getResult();
-private:
-    
 };
 
 #endif
