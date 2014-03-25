f79cce0bba52d1b2d97307315d691116697cdb81
jerzozwierz <tedzio44@gmail.com>
2014-03-25 16:06:23 +0100

    Started implementing "Bidding" class + little changes in "Trump"

--- src/Bidding.hpp
+++ src/Bidding.hpp
@@ -1,17 +1,41 @@
 #ifndef BIDDING_HPP
 #define  BIDDING_HPP
+
+#include <vector>
+#include <utility>
+
+
 class Bidding
 {
 public:
 	Bidding();
-
+	
 	bool makeCall(Call const& call);
+	
 	Contract getContract();
-	int getDealer();
+	
 	BiddingConstraint getCurrentConstraint();
 	
-private:
+	bool stillGoing();
 	
-
-};
+private:
+	int whoBidsNow; /* 0, 1, 2, 3 */
+	int passCount; /* how many consecutive passes at the end */
+	Call lastNonPass; /* last non-pass call */
+	std::vector<std::pair<int, Call>> history;
+	
+	void next() 
+	{
+		++whoBidsNow;
+		whoBidsNow %= 4;
+	}
+	
+	void archive(Call const & call) 
+	{
+		history.push_back(std::make_pair(whoBidsNow, call));
+	}
+	
+	
+	
+}
 #endif // BIDDING_HPP
