a4b5b88d243567750a034adf79b742ff982e20db
Teodor Jerzak <tedzio44@gmail.com>
2014-04-01 15:47:58 +0200

    implementation of Bidding class

--- src/Bidding.cpp
+++ src/Bidding.cpp
@@ -1,21 +1,56 @@
 #include "Bidding.hpp"
 
+Bidding::Bidding() : whoBidsNow(0), passCount(0)
+{
+	lastNonPass = Call::createPass();
+	lastActual = Call::createPass();
+}
+
 bool Bidding::makeCall(Call const & call)
 {
-	
+	if (!stillGoing)
+		throw 666;
+	if (!getCurrentConstraint().satisfies(call))
+		return false;
+	/* all right, call is legal */
+	switch (call.type) {
+		case PASS: // nothing interesting
+			++passCount;
+			break;
+		case DOUBLE:
+		case REDOUBLE: // these cases don't differ
+			passCount = 0;
+			lastNonPass = call;
+			whoDidIt = currentPair();
+			break;
+		case STANDARD: 
+			passCount = 0;
+			lastNonPass = call;
+			lastActual = call;
+			whoDidIt = currentPair();
+	}
+	archive(call);
+	next();
 }
 
-Contract Bidding::getContract()
+const Contract Bidding::getContract()
 {
+	if (stillGoing)
+		throw 666;
 	
 }
 
-BiddingConstraint Bidding::getCurrentConstraint()
+const BiddingConstraint Bidding::getCurrentConstraint()
 {
 	
 }
 
 bool Bidding::stillGoing()
 {
-	
+	/* a bit ugly, I'm sorry */
+	if (passCount == 4)
+		return false;
+	if (passCount == 3 && lastNonPass.type != CallType::PASS)
+		return false;
+	return true;
 }
