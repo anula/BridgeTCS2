2a5b962a78a325d218885b157cc4b7fa559664fb
mreve <aadymek@gmail.com>
2014-04-29 15:27:21 +0200

    DummyComputerPlayer.* polished

--- src/DummyComputerPlayer.hpp
+++ src/DummyComputerPlayer.hpp
@@ -1,13 +1,13 @@
 #ifndef DUMMY_COMPUTER_PLAYER_HPP
 #define DUMMY_COMPUTER_PLAYER_HPP
 
-#include <IPlayer.hpp>
+#include "IPlayer.hpp"
 
 class DummyComputerPlayer : IPlayer
 {
 public:
 	int getCard(std::vector<CardPtr> & hand); 
 	Call getCall();
-}
+};
 
 #endif
