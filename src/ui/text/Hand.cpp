420b4367f37d6e58b946e41222f88316dbcfffaa
Krzysztof Baranski <krzysztof.baranski@uj.edu.pl>
2014-05-27 15:35:05 +0200

    zmiana w hand i deal

--- src/ui/text/Hand.cpp
+++ src/ui/text/Hand.cpp
@@ -1,12 +1,9 @@
 #include "Hand.hpp"
 #include "Printer.hpp"
 
-const char ui::text::Hand::ranks[] = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
-const std::string ui::text::Hand::suits[] = {"\u2663", "\u2666", "\u2665", "\u2660"};
-const std::string ui::text::Hand::trumps[] = {"\u2663", "\u2666", "\u2665", "\u2660", "NT"};
-
-void ui::text::Hand::notify(model::Hand const & target) 
+void ui::text::Hand::notify(model::Hand const & hand) 
 {
-	Printer::print(target);
+	std::cout << text << std::endl;
+	Printer::print(hand);
 }
 
