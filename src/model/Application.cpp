e1fd2cc5d1d1ad24b17ea05cb9d7e3f54290cfbf
laiqu <laiquendir@gmail.com>
2014-06-14 14:49:50 +0200

    Added pseudo main menu!

--- src/model/Application.cpp
+++ src/model/Application.cpp
@@ -6,14 +6,16 @@
 
 void model::Application::run()
 {
-    DummyComputerPlayer dummy1;
-    DummyComputerPlayer dummy2;
-    DummyComputerPlayer dummy3;
-    ui::text::Player user;
-    game = std::unique_ptr<IGame>(new SimpleGame(dummy1, dummy2, dummy3, user));
-	state = STARTED;
-    sigModified(*this);
-    game->start();
-	state = FINISHED;
-    sigModified(*this);
+    while(true) {
+        DummyComputerPlayer dummy1;
+        DummyComputerPlayer dummy2;
+        DummyComputerPlayer dummy3;
+        ui::text::Player user;
+        game = std::unique_ptr<IGame>(new SimpleGame(dummy1, dummy2, dummy3, user));
+        state = STARTED;
+        sigModified(*this);
+        game->start();
+        state = FINISHED;
+        sigModified(*this);
+    }
 }
--- src/ui/text/Application.cpp
+++ src/ui/text/Application.cpp
@@ -3,18 +3,25 @@
 void ui::text::Application::notify(model::Application const & app) 
 {
 	if (app.state == model::Application::State::STARTED) {
-		std::cout << indent << "Zapraszamy do gry!" << std::endl;
-		app.getGame().sigModified.connect(
-				[this] (model::IGame const & game) {
-					gameView.notify(game);
-				}
-			);
+		std::cout << indent << "Welcome!" << std::endl;
+        std::cout << indent << "Type :quit to exit BridgeTCS. Enter anything else to start the game!" << std::endl;
+        std::string tmp;
+        std::cin >> tmp;
+        if(tmp != ":quit") {
+            app.getGame().sigModified.connect(
+                [this] (model::IGame const & game) {
+                    gameView.notify(game);
+                }
+            );
+        } else {
+            exit(0);
+        }
 	} else if (app.state == model::Application::State::FINISHED) {
 		std::cout << "Contract: " << std::endl;
 		ui::text::Printer::print(app.getGame().result.contract);
 		std::cout << std::endl;
 		std::cout << "Tricks collected by declarer and his partner: " 
 			<< app.getGame().result.tricksCollected << std::endl;
-		std::cout << indent << "Dziękujemy za grę!" << std::endl;
+		std::cout << indent << "Thanks for playing!" << std::endl;
 	}
 }
